3f11783dafb8d95f4fb28a4e94f1c4cdcadfa43a
Salématou Kounta <salematoukounta@Mac.lan>
2025-02-10 06:46:08 +0100

    feat: implementation de la feat say hello

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import unittest
+from app import say_hello
+
+class TestApp(unittest.TestApp):
+    def test_say_hello(self):
+        self.assertEqual(say_hello("AWS"), "Hello, AWS")
+
+if __name__ == "__main__":
+    unittest.main()
